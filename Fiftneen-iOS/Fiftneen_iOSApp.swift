bbca85e798e8d8fe1794485195853517e2116b92
Onnwen <onnwen.cassitto@icloud.com>
2023-01-19 18:23:06 +0100

    Initial Commit

---
+++ Fiftneen-iOS/Fiftneen_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fiftneen_iOSApp.swift
+//  Fiftneen-iOS
+//
+//  Created by Onnwen Cassitto on 19/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Fiftneen_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
